939d6bb42623100a7ee7f07c21ba99e01aff1096
Eric Wagner <eric.wagner@geneticfinance.com>
2022-11-16 16:11:52 -0800

    web_form.go

---
+++ web_form.go
@@ -0,0 +1,41 @@
+package main
+
+import "net/http"
+
+// html form
+// first name
+// last name
+// phone
+
+func serveForm(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Content-Type", "text/html")
+		w.Write([]byte(`
+			<form method="POST">
+				<input type="text" name="first_name">
+				<input type="text" name="last_name">
+				<input type="text" name="phone">
+				<input type="submit">
+			</form>
+		`))
+		return
+	}
+
+	r.ParseForm()
+	w.Write([]byte(r.FormValue("first_name")))
+	w.Write([]byte(r.FormValue("last_name")))
+	w.Write([]byte(r.FormValue("phone")))
+}
+
+func processForm(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	w.Write([]byte(r.FormValue("first_name")))
+	w.Write([]byte(r.FormValue("last_name")))
+	w.Write([]byte(r.FormValue("phone")))
+}
+
+func main() {
+	http.HandleFunc("/", serveForm)
+	http.HandleFunc("/process", processForm)
+	http.ListenAndServe(":8080", nil)
+}
